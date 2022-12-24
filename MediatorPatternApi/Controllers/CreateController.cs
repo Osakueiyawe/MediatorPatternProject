d077ec8fabccc8b9f7a1cec58ce8516935e8bce1
OSAKUE IYAWE <osakueiyawe@gmail.com>
2022-12-24 15:15:58 +0100

    Initial commit

---
+++ MediatorPatternApi/Controllers/CreateController.cs
@@ -0,0 +1,32 @@
+﻿using MediatorPatternApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace MediatorPatternApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateController : ControllerBase
+    {
+        private IMediator _mediator;
+        public CreateController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRequest request)
+        {
+            CreateResponse response = null;
+            try
+            {
+                response = await _mediator.Send(request);
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }            
+        }
+    }
+}
---
+++ MediatorPatternApi/Services/CreateService.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+namespace MediatorPatternApi.Services
+{
+    public class CreateRequest:IRequest<CreateResponse>
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+
+    public class CreateResponse
+    {
+        public string responseCode { get; set; }
+        public string responseMessage { get; set; }
+    }
+    public class CreateService:IRequestHandler<CreateRequest,CreateResponse>
+    {
+        public async Task<CreateResponse> Handle(CreateRequest request, CancellationToken cancellationToken)
+        {
+            CreateResponse response = new CreateResponse();
+            try
+            {
+                if(request.id == "1")
+                {
+                    response.responseCode = "00";
+                    response.responseMessage = "successful";
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return response;
+        }
+    }
+}
